b2201b88c13c7737cfa946a4c89ae6633394e6d2
jlo2224 <jaredlo@hawaii.edu>
2025-09-20 13:39:01 -1000

    declared module

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,3 +1,5 @@
+declare module "@aurodesignsystem/auro-header";
+
 declare namespace JSX {
   interface IntrinsicElements {
     'auro-header': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
